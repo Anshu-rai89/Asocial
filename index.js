62256a6145d26f6be9e00182360e14165e9fdf78
Anshu <anshurai8991@gmail.com>
2019-11-09 14:05:07 +0530

    create express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+
+
+const app=express();
+const port=8000;
+
+
+// firing server
+
+app.listen(port,function(err)
+{
+     if(err)  {console.log(`Error in running server:${port}`);return;}
+
+     console.log(`Surver is up and Running at POrt :${port}`); return;
+});
