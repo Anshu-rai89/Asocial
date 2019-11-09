58d863d27397f9a4839eb27e5f77d17f3df5e885
Anshu <anshurai8991@gmail.com>
2019-11-09 23:04:35 +0530

    conntected with database

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+mongoose.connect('mongodb://localhost/codial_development');
+
+const db=mongoose.connection;
+
+db.on('error',console.error.bind(console,'Error in connecting to database'));
+
+db.once('open',function()
+{
+    console.log("Connected to Database :DB");
+});
--- index.js
+++ index.js
@@ -6,8 +6,14 @@ const port=8000;
 
 // using express layouts for creating views layout
 const expresslayout=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+app.use(express.static('./assets'));
 
 app.use(expresslayout);
+
+// extract style and script from subpages to layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 
 // using express router
