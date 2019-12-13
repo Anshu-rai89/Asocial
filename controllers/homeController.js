f78bfe1d5f04dbb07e8ca451b3dff811d7b7da83
Anshu Rai <raianshu8991@gmail.com>
2019-12-13 02:06:30 -0500

    added post

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,13 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req,res)
-{
-    res.render('home',{title:"home"});
+{    Post.find({}).populate('user').exec(function(err,posts)
+      {  
+          if(err){console.log("Erro in finding post");return;}
+           res.render('home',
+           {
+               title:"Asocial | home",
+               posts:posts
+           });
+});
 }
