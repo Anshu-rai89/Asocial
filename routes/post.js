11c52c1e57e56706958860be2f00baeae7a393e5
Anshu Rai <raianshu8991@gmail.com>
2019-12-13 02:31:32 -0500

    added authentication to post

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const passport=require('../config/passport-local');
 
 // using express route
 const router=express.Router();
@@ -6,6 +7,6 @@ const router=express.Router();
 const postcontrooler=require('../controllers/postcontroller');
 
 
-router.post('/create',postcontrooler.create);
+router.post('/create',passport.checkAuthentication,postcontrooler.create);
 
 module.exports=router;
