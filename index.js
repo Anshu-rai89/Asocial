e056205f5a8498f9eb4d6f414fe57b3a189cc030
Ankit Kumar <hello@ankdev.me>
2020-01-13 19:14:54 +0530

    delete button padding fix

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ console.log('chatserver is listning at port 5000');
 if (env.name == 'development')
 {
     app.use(sassMiddleware({
-        src: './public/assets/scss',
-        dest: './public/assets/css',
+        src: './assets/scss',
+        dest: './assets/css',
         debug: true,
         outputStyle: 'compressed',
         prefix: '/css'
@@ -47,6 +47,13 @@ if (env.name == 'development')
     app.use(express.static('./assets'));
 
 } else {
+    app.use(sassMiddleware({
+        src: './assets/scss',
+        dest: '.public/assets/css',
+        debug: true,
+        outputStyle: 'compressed',
+        prefix: '/css'
+    }));
     app.use(express.static('./public/assets'));
 }
 
